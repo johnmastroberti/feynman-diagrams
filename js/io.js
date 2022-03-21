ac806b84310b9424e82e764b0cef91b120efb03a
John Mastroberti <johnmastroberti123@gmail.com>
2022-03-21 10:14:24 -0400

    latex export

--- js/io.js
+++ js/io.js
@@ -17,7 +17,11 @@ function exportPNG() {
   downloadData("diagram.png", $("#drawingArea")[0].toDataURL("image/png"));
 }
 
-function exportTEX() {}
+function exportText(name, text) {
+  const blob = new Blob([text], { type: "application/json" });
+  const url = window.URL.createObjectURL(blob);
+  downloadData(name, url);
+}
 
 function exportJSON() {
   const appState = {
@@ -27,10 +31,7 @@ function exportJSON() {
     nextID: _nextID,
   };
 
-  const text = JSON.stringify(appState);
-  const blob = new Blob([text], { type: "application/json" });
-  const url = window.URL.createObjectURL(blob);
-  downloadData("diagram.json", url);
+  exportText("diagram.json", JSON.stringify(appState));
 }
 
 function loadAppState(appState) {
@@ -71,4 +72,70 @@ function importJSON() {
   });
 
   fileInput.click();
+}
+
+function vertexTEX(v) {
+  const vertOpts = {
+    Normal: "",
+    Blob: "[blob]",
+    "1PI": "[blob]",
+    Insertion: "[crossed dot]",
+    Counterterm: "[crossed dot]",
+  };
+  let opt = vertOpts[v.type];
+  if (opt === undefined) opt = "";
+  const end = opt != "" ? "{}" : "";
+  return `\\vertex${opt} (v${v.id}) at (${v.x / 100}, ${
+    (800 - v.y) / 100
+  }) ${end};\n`;
+}
+
+function edgeTEX(e) {
+  const lineTypes = {
+    Sold: "plain",
+    Dashed: "scalar",
+    Fermion: "fermion",
+    "Anti-Fermion": "anti fermion",
+    Photon: "photon",
+    Gluon: "gluon",
+    Ghost: "ghost",
+    Double: "double",
+  };
+  let lineOpt = lineTypes[e.type];
+  if (lineOpt === undefined) lineOpt = "plain";
+
+  const curveOpt = e.curve ? ", half left" : "";
+  const mod1 = e.reverseMomentum ? "r" : "";
+  const mod2 = e.swapMomentumSide ? "'" : "";
+  const momOpt = e.momentumArrow ? `, ${mod1}momentum${mod2}={}` : "";
+
+  const opts = `[${lineOpt}${curveOpt}${momOpt}]`;
+
+  return `(v${e.v1}) --${opts} (v${e.v2}),\n`;
+}
+
+function exportTEX() {
+  // header
+  let tex = `\\documentclass{standalone}
+\\usepackage{amsmath, amssymb, gensymb, tikz}
+\\usepackage[compat=1.1.0]{tikz-feynman}
+\\usepackage{contour}
+\\begin{document}
+\\begin{tikzpicture}
+\\begin{feynman}\n`;
+
+  // vertices
+  for (let v of vertices) tex += vertexTEX(v);
+
+  // edges
+  tex += "\\diagram* {\n";
+  for (let e of edges) tex += edgeTEX(e);
+  tex += "};\n\\end{feynman}\n";
+
+  // labels
+  for (let l of labels)
+    tex += `\\node (${l.x / 100}, ${(800 - l.y) / 100}) {$${l.text}$};\n`;
+
+  tex += "\\end{tikzpicture}\n\\end{document}";
+  exportText("diagram.tex", tex);
 }
